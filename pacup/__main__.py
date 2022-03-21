f9d74c6f43e65c5915ae9820c80ec5becf97c399
Sourajyoti Basak <wiz28@protonmail.com>
2022-03-21 21:48:45 +0530

    build: release 0.1.2 Thyone

--- pacup/__main__.py
+++ pacup/__main__.py
@@ -64,7 +64,7 @@ from rich.table import Table
 from pacup.parser import Pacscript
 from pacup.version import VersionStatuses
 
-__version__ = "0.1.2 Thyone (dev)"
+__version__ = "0.1.2 Thyone"
 
 app = typer.Typer(name="pacup")
 
