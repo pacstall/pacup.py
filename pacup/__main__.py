1bedf15481cde0d57eb2c0ecb35c9abbdd1edaa1
Sourajyoti Basak <wiz28@protonmail.com>
2022-03-22 11:20:45 +0530

    build: setup `1.0.0` Betelgeuse (dev)

--- pacup/__main__.py
+++ pacup/__main__.py
@@ -64,7 +64,7 @@ from rich.table import Table
 from pacup.parser import Pacscript
 from pacup.version import VersionStatuses
 
-__version__ = "0.1.2 Thyone"
+__version__ = "1.0.0 Betelgeuse (dev)"
 
 app = typer.Typer(name="pacup")
 
